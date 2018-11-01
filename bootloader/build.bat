be7aeac739139604d4e892690ea3ff5012bcd901
paulscottrobson <paulscottrobson@gmail.com>
2018-11-01 20:17:05 +0000

    First Commit

---
+++ bootloader/build.bat
@@ -0,0 +1,17 @@
+@echo off
+rem
+rem	Remove SNA files
+rem
+del /Q bootloader.sna 
+del /Q ..\files\bootloader.sna
+rem
+rem	Build SNA file
+rem
+..\bin\snasm bootloader.asm
+del /Q bootloader.asm.dat
+rem
+rem	Copy to files area if successful.
+rem
+if exist bootloader.sna	copy bootloader.sna ..\files
+
+
